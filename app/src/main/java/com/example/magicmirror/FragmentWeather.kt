3f1b56c826107f71fb5ba14ff435267f0b4f5280
sandyz987 <1409618590@qq.com>
2021-03-08 11:50:36 +0800

    天气定位

--- app/src/main/java/com/example/magicmirror/FragmentWeather.kt
+++ app/src/main/java/com/example/magicmirror/FragmentWeather.kt
@@ -8,6 +8,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.amap.api.location.AMapLocationClient
+import com.amap.api.location.AMapLocationClientOption
+import com.amap.api.location.AMapLocationListener
 import com.amap.api.navi.AMapNavi
 import com.amap.api.services.weather.LocalWeatherForecastResult
 import com.amap.api.services.weather.LocalWeatherLiveResult
@@ -26,8 +29,64 @@ class FragmentWeather : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        //声明AMapLocationClient类对象
+        //声明AMapLocationClient类对象
+        val mLocationClient: AMapLocationClient?
+        //声明定位回调监听器
+        //声明定位回调监听器
+        val mLocationListener = AMapLocationListener {
+            playWeather(it.adCode)
+            Log.e("sandyzhang", it.adCode)
+        }
+        //初始化定位
+        //初始化定位
+        mLocationClient = AMapLocationClient(activity!!)
+        //设置定位回调监听
+        //设置定位回调监听
+        mLocationClient.setLocationListener(mLocationListener)
+        //声明AMapLocationClientOption对象
+        //声明AMapLocationClientOption对象
+        val mLocationOption: AMapLocationClientOption?
+        //初始化AMapLocationClientOption对象
+        //初始化AMapLocationClientOption对象
+        mLocationOption = AMapLocationClientOption()
+
+        val option = AMapLocationClientOption()
+        /**
+         * 设置定位场景，目前支持三种场景（签到、出行、运动，默认无场景）
+         */
+        /**
+         * 设置定位场景，目前支持三种场景（签到、出行、运动，默认无场景）
+         */
+        option.locationPurpose = AMapLocationClientOption.AMapLocationPurpose.SignIn
+        mLocationClient.setLocationOption(option)
+        //设置场景模式后最好调用一次stop，再调用start以保证场景模式生效
+        mLocationClient.stopLocation()
+        mLocationClient.startLocation()
+
+        //设置定位模式为AMapLocationMode.Battery_Saving，低功耗模式。
+        mLocationOption.locationMode = AMapLocationClientOption.AMapLocationMode.Battery_Saving
+        //获取一次定位结果：
+        //该方法默认为false。
+        mLocationOption.setOnceLocation(true)
+
+        //获取最近3s内精度最高的一次定位结果：
+        //设置setOnceLocationLatest(boolean b)接口为true，启动定位时SDK会返回最近3s内精度最高的一次定位结果。如果设置其为true，setOnceLocation(boolean b)接口也会被设置为true，反之不会，默认为false。
+        mLocationOption.setOnceLocationLatest(true)
+
+        //给定位客户端对象设置定位参数
+        mLocationClient.setLocationOption(mLocationOption)
+        //启动定位
+        mLocationClient.startLocation()
+
+
+
+
+    }
+    fun playWeather(cityCode: String) {
         //检索参数为城市和天气类型，实况天气为WEATHER_TYPE_LIVE、天气预报为WEATHER_TYPE_FORECAST
-        val mquery = WeatherSearchQuery("110000", WeatherSearchQuery.WEATHER_TYPE_LIVE)
+        val mquery = WeatherSearchQuery(cityCode, WeatherSearchQuery.WEATHER_TYPE_LIVE)
         val mweathersearch = WeatherSearch(activity)
         mweathersearch.setOnWeatherSearchListener(object : WeatherSearch.OnWeatherSearchListener {
             @SuppressLint("SetTextI18n")
@@ -61,4 +120,5 @@ class FragmentWeather : Fragment() {
         mweathersearch.setQuery(mquery)
         mweathersearch.searchWeatherAsyn() //异步搜索
     }
+
 }
